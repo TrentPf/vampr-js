90985f22cb1b6c35e93e0574d839b5112adf048e
Trent Pfeiffer <trentpf@hotmail.com>
2021-11-12 13:56:31 -0500

    Add vampire class and functions

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -36,7 +49,44 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+      const difference = this.numberOfVampiresFromOriginal - vampire.numberOfVampiresFromOriginal;
+      let currentVampire = this;
+      let currentVampire2 = vampire;
+      
+      for (let i = 0; i < difference; i++) {
+        currentVampire = currentVampire.creator;
+      }
 
+      while (currentVampire !== currentVampire2) {
+        currentVampire = currentVampire.creator;
+        currentVampire2 = currentVampire2.creator;
+      }
+      return currentVampire;
+    } else if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      const difference = vampire.numberOfVampiresFromOriginal - this.numberOfVampiresFromOriginal;
+      let currentVampire = vampire;
+      let currentVampire2 = this;
+
+      for (let i = 0; i < difference; i++) {
+        currentVampire = currentVampire.creator;
+      }
+
+      while (currentVampire !== currentVampire2) {
+        currentVampire = currentVampire.creator;
+        currentVampire2 = currentVampire2.creator;
+      }
+      return currentVampire;
+    } else if (this.numberOfVampiresFromOriginal === vampire.numberOfVampiresFromOriginal) {
+      let currentVampire = this;
+      let currentVampire2 = vampire;
+      
+      while (currentVampire !== currentVampire2) {
+        currentVampire = currentVampire.creator;
+        currentVampire2 = currentVampire2.creator;
+      }
+      return currentVampire;
+    }
   }
 }
 
